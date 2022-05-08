67d77013d0d829ca42602971f59936b39d840ea7
Arman Karimov <krmv.kz@gmail.com>
2022-05-08 12:35:31 +0600

    Initial Commit

---
+++ ProgrammaticUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIKit
+//
+//  Created by Arman Karimov on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
